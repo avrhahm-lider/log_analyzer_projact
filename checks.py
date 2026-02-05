104847718ebf637ce81956ab78b8d0890c9e0cdd
avrhahm <am500148148@gmail.com>
2026-02-05 17:11:34 +0200

    add ip funcktion

--- checks.py
+++ checks.py
@@ -1,18 +1,19 @@
+import csv
 from pathlib import Path
 def extract_cvs_file(file_path):
-    send_list =[]
-    p = Path(file_path)
-    txt = p.read_text()
-    with open("csv_file.csv","w") as f:
-        f.write(txt)
     try:
-        with open("csv_file.csv", "r") as f1:
-            sockets_list = f1.readlines()
-            for socket in sockets_list:
-                socket = socket[:-2]
-                send_list.append(socket.split(","))
-            return send_list
+        with open(file_path, "r") as f:
+            r = csv.reader(f)
+            return [row for row in r]
     except FileNotFoundError as e:
         print(f"Error {e}")
 path = "C:\\Users\\1234\Downloads\log_analyzer_project-main\\network_traffic.log"
 print(extract_cvs_file(path))
+def external_ip_extraction(data: list[list[str]]):
+    ip_extrac =[]
+    for socket in data:
+        ip = socket[1].split(".")
+        if ip[0] != "10" and ip[0]+ip[1] != "192168":
+            ip_extrac.append(ip)
+    return ip_extrac
+print(external_ip_extraction(extract_cvs_file(path)))
