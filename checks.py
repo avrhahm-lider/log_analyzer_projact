063f6d8f02659a3dd027ff488409fa7f9a2f6656
avrhahm <am500148148@gmail.com>
2026-02-07 21:18:01 +0200

    add 3 function

--- checks.py
+++ checks.py
@@ -8,7 +8,7 @@ def extract_cvs_file(file_path):
     except FileNotFoundError as e:
         print(f"Error {e}")
 path = "C:\\Users\\1234\Downloads\log_analyzer_project-main\\network_traffic.log"
-print(extract_cvs_file(path))
+
 def external_ip_extraction(data: list[list[str]]):
     ip_extrac =[]
     for socket in data:
@@ -16,4 +16,24 @@ def external_ip_extraction(data: list[list[str]]):
         if ip[0] != "10" and ip[0]+ip[1] != "192168":
             ip_extrac.append(ip)
     return ip_extrac
-print(external_ip_extraction(extract_cvs_file(path)))
+print(len(extract_cvs_file(path)))
+def external_sensitive_ports(data: list[list[str]]):
+    sensitive_ports = {"22","3389","23"}
+    return [socket for socket in data if socket[-3] in sensitive_ports]
+print(len(external_sensitive_ports(extract_cvs_file(path))))
+
+def external_big_packets(data: list[list[str]]):
+    return [socket for socket in data if int(socket[-1]) > 5000]
+
+
+def package_labeling(data: list[list[str]]):
+    labeling_list = []
+    for socket in data:
+        if int(socket[-1]) > 5000:
+            socket.append("LARGE")
+            labeling_list.append(socket)
+        else:
+            socket.append("NORMAL")
+            labeling_list.append(socket)
+    return labeling_list
+print(package_labeling(extract_cvs_file(path)))
