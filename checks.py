ecb05a53163570d909baf6434baa580a56d3ba25
avrhahm <am500148148@gmail.com>
2026-02-08 11:31:13 +0200

    chancge all function to list comprhension

--- checks.py
+++ checks.py
@@ -10,30 +10,14 @@ def extract_cvs_file(file_path):
 path = "C:\\Users\\1234\Downloads\log_analyzer_project-main\\network_traffic.log"
 
 def external_ip_extraction(data: list[list[str]]):
-    ip_extrac =[]
-    for socket in data:
-        ip = socket[1].split(".")
-        if ip[0] != "10" and ip[0]+ip[1] != "192168":
-            ip_extrac.append(ip)
-    return ip_extrac
-print(len(extract_cvs_file(path)))
+    return [socket[1] for socket in data if socket[1].split(".")[0] != "10" and socket[1].split(".")[0]+socket[1].split(".")[1] != "192168" ]
+
 def external_sensitive_ports(data: list[list[str]]):
     sensitive_ports = {"22","3389","23"}
     return [socket for socket in data if socket[-3] in sensitive_ports]
-print(len(external_sensitive_ports(extract_cvs_file(path))))
 
 def external_big_packets(data: list[list[str]]):
     return [socket for socket in data if int(socket[-1]) > 5000]
 
-
 def package_labeling(data: list[list[str]]):
-    labeling_list = []
-    for socket in data:
-        if int(socket[-1]) > 5000:
-            socket.append("LARGE")
-            labeling_list.append(socket)
-        else:
-            socket.append("NORMAL")
-            labeling_list.append(socket)
-    return labeling_list
-print(package_labeling(extract_cvs_file(path)))
+    return [(socket,"LARGE") if int(socket[-1]) > 5000 else (socket,"NORMAL") for socket in data]
